b054804b624384d864910a86bd65535d8eefbc52
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 16:43:34 -0300

    Basic calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,17 +6,54 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Sum();
-        }
-        
-        static void Sum()
-        {
             Console.Clear();
-            Console.WriteLine("Digite o primeiro valor: ");
+            Console.Write("Digite o operador: ");
+
+            string switchCase = Console.ReadLine();
+
+            Console.Write("Digite o primeiro valor: ");
             float v1 = float.Parse(Console.ReadLine());
             Console.Write("Digite o segundo valor: ");
             float v2 = float.Parse(Console.ReadLine());
+
+            switch(switchCase)
+            {
+                case "+":
+                    Sum(v1, v2);
+                    break;
+                case "-":
+                    Sub(v1, v2);
+                    break;
+                case "*":
+                    Mult(v1, v2);
+                    break;
+                case "/":
+                    Div(v1, v2);
+                    break;
+                default:
+                    Console.WriteLine("Operator doesn't exist");
+                    break;
+            }
+        }
+        
+        static void Sum(float v1, float v2)
+        {
             Console.WriteLine($"O resultado da soma de {v1} + {v2} é: {v1 + v2}");
+        }
+
+        static void Sub(float v1, float v2)
+        {
+            Console.WriteLine($"O resultado da subtração de {v1} - {v2} é: {v1 - v2}");
+        }
+
+        static void Mult(float v1, float v2)
+        {
+            Console.WriteLine($"O resultado da multiplicação de {v1} * {v2} é: {v1 * v2}");
+        }
+
+        static void Div(float v1, float v2)
+        {
+            Console.WriteLine($"O resultado da divisão de {v1} / {v2} é: {v1 / v2}");
         }
     }
 }
