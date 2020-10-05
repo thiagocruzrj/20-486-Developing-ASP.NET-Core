c2343616231dd2996fe2275ea2443671b33cd891
Thiago Cruz <thagocruz@gmail.com>
2020-10-05 16:37:18 -0300

    add base project

---
+++ PipelinesWithTests/TUnidade/TUnidade/Util.cs
@@ -0,0 +1,14 @@
+﻿namespace TUnidade
+{
+    public static class Util
+    {
+        public static bool PrimeiraLetraMaiuscula(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
