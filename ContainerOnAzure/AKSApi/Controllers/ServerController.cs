dddac9dcd75f141d5fdb76c572f2ba9b1d3284f0
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 16:49:42 -0300

    Add aks api

---
+++ ContainerOnAzure/AKSApi/Controllers/ServerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AKSApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ServerController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ServerController> _logger;
+
+        public ServerController(ILogger<ServerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Nome maquina: " + Environment.MachineName + "| SO: " + Environment.OSVersion.VersionString;
+        }
+    }
+}
